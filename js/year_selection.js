cd62ab9bbaaa8018ffa19bd4fe962a050d041918
SadhwaniMoomal <sadhwanimoomal987@gmail.com>
2024-09-27 17:20:25 +0500

    modify  floder

--- js/year_selection.js
+++ js/year_selection.js
@@ -9,7 +9,7 @@ document.getElementById('firstYearBtn').addEventListener('click', function() {
     // Show the check icon and redirect after 2 seconds
     document.getElementById('checkIcon').classList.add('active');
     setTimeout(function() {
-        window.location.href = 'categories.html'; // Replace with your next page URL
+        window.location.href = '../firstYear/First_categories.html'; // Replace with your next page URL
     }, 2000);
 });
 
@@ -17,6 +17,6 @@ document.getElementById('secondYearBtn').addEventListener('click', function() {
     flipContainer(); // Flip effect
     // Redirect to second year page after 2 seconds
     setTimeout(function() {
-        window.location.href = 'second-year.html'; // Replace with your second-year page URL
+        window.location.href = '../Secondyear/second_categories.html'; // Replace with your second-year page URL
     }, 2000);
 });
